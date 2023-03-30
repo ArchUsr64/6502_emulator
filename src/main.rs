c8d704de068ed2878b9ca0802e29e2a851c1ad0f
ArchUsr64 <anshulusr@gmail.com>
2023-03-30 18:20:13 +0530

    added miniquad

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 mod cpu;
 use cpu::*;
 
-fn main() {
-	let mut mem = read_mem("a.out");
+use macroquad::prelude::*;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+	let data = read_mem("a.out");
+	let mut mem = Memory::new(data);
 	let mut cpu = Cpu::new();
 	cpu.reset(&mem);
 	println!("Reset the CPU: {cpu:?}");
 	loop {
-		mem.data[0xfe] = rand::random();
+		mem.data[0xfe] = rand::gen_range(0, u8::MAX);
 		cpu.execute(&mut mem);
 		println!("{:02x?}", &mem.data[0x200..0x300]);
 		println!("{cpu:?}");
-		// println!("{:02x?}", mem[0xfe]);
-		// println!("{:02x?}", mem[0x200]);
 	}
 }
 
-fn read_mem(file_path: &'static str) -> Memory {
+fn read_mem(file_path: &'static str) -> [u8; MEMORY_SIZE] {
 	let rom = std::fs::read(file_path).unwrap();
-	let data = [0; MEMORY_SIZE];
-	let mut mem = Memory::new(data);
+	let mut data = [0; MEMORY_SIZE];
 	assert_eq!(rom.len(), MEMORY_SIZE, "Invalid ROM size");
 	for (index, val) in rom.iter().enumerate() {
-		mem.data[index] = *val;
+		data[index] = *val;
 	}
-	mem
+	data
 }
