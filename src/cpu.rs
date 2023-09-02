afb0304ee51f0cd713b00259d454484f4e7ba2e9
Anshul <anshulusr@gmail.com>
2023-09-02 12:20:07 +0530

    changed `STACK_LOWEST_ADDRESS` to match 6502

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-const STACK_LOWEST_ADDRESS: u16 = 0xff << 2;
+const STACK_LOWEST_ADDRESS: u16 = 0x100;
 pub const MEMORY_SIZE: usize = 0x10000;
 pub struct Memory {
 	pub data: [u8; MEMORY_SIZE],
